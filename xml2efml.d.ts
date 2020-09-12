8220d1689b965927cecb48a2b81602de309a9641
tcdw <tcdw2011@gmail.com>
2020-09-12 10:02:29 +0800

    upgrade dep & add d.ts

---
+++ xml2efml.d.ts
@@ -0,0 +1,41 @@
+export function dom2efml(elem: Element, { spaces, ignoreEmptyTextNode, indentOffset, }?: {
+    spaces?: string;
+    ignoreEmptyTextNode?: boolean;
+    indentOffset?: number;
+}): string;
+
+export function dom2ast(elem: Element, { ignoreEmptyTextNode, }?: {
+    ignoreEmptyTextNode?: boolean;
+}): {
+    t: string;
+}[];
+
+export function htmlSnippet2efml(str: string, { spaces, ignoreEmptyTextNode, win, }?: {
+    spaces?: string;
+    ignoreEmptyTextNode?: boolean;
+    win: Window;
+}): string;
+
+export function htmlSnippet2ast(str: string, { ignoreEmptyTextNode, win, }?: {
+    ignoreEmptyTextNode?: boolean;
+    win: Window;
+}): {
+    t: string;
+}[] | {
+    t: number;
+}[];
+
+function xml2efml(str: string, { spaces, ignoreEmptyTextNode, win, type, }?: {
+    spaces?: string;
+    ignoreEmptyTextNode?: boolean;
+    win: Window;
+    type?: string;
+}): string;
+
+function xml2ast(str: string, { ignoreEmptyTextNode, win, type, }?: {
+    ignoreEmptyTextNode?: boolean;
+    win: Window;
+    type?: string;
+}): {
+    t: string;
+}[];
